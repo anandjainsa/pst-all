c4f38b0469656ce788d943f8acd2d07f351079ca
Anand Jain <anandjainsa@gmail.com>
2020-04-24 08:36:23 +0000

    updated release tags

--- vars/mavenReleaseTag.groovy
+++ vars/mavenReleaseTag.groovy
@@ -1,24 +1,25 @@
 def call(MVN_DEV_VERSION, MVN_RELEASE_VERSION, gitbranch, GIT_PASS)
      {
           sh '''
-#		git clone https://github.com/anandjainsa/scripts.git /tmp/releasescripts
-		git clone git@github.com:anandjainsa/scripts.git /tmp/releasescripts
-		sleep 5
-		export GIT_PASSWORD="${GIT_PASS}"
-		export GIT_ASKPASS="/tmp/releasescripts/gitpass.sh"
- 
-		if [ -z ${MVN_DEV_VERSION} ] || [ -z ${MVN_RELEASE_VERSION} ]
- 
-            	then
-            		exit 0;
-		fi
- 
-		if [ -d /tmp/releasescripts ]
-		 then
-           	   rm -rf /tmp/releasescripts
-		fi
- 
-		sh /tmp/releasescripts/gitrelease.sh ${MVN_RELEASE_VERSION} ${MVN_DEV_VERSION} ${gitbranch}
- 	'''
+#               git clone https://github.com/anandjainsa/scripts.git /tmp/releasescripts
+#               git clone git@github.com:anandjainsa/scripts.git /tmp/releasescripts
+                sleep 5
+                export GIT_PASSWORD="${GIT_PASS}"
+                export GIT_ASKPASS="/tmp/release/gitpass.sh"
+
+                if [ -z ${MVN_DEV_VERSION} ] || [ -z ${MVN_RELEASE_VERSION} ]
+
+                then
+                        exit 0;
+                fi
+
+                if [ -d /tmp/releasescripts ]
+                 then
+                   rm -rf /tmp/releasescripts
+                fi
+                git clone git@github.com:anandjainsa/scripts.git /tmp/releasescripts
+
+                sh /tmp/releasescripts/gitrelease.sh ${MVN_RELEASE_VERSION} ${MVN_DEV_VERSION} ${gitbranch}
+        '''
 }
 
