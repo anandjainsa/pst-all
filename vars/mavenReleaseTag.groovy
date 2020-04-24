2e850d8d528202569c491e685738ef8b62aecac3
Anand Jain <anandjainsa@gmail.com>
2020-04-24 08:11:20 +0000

    added git ssh

--- vars/mavenReleaseTag.groovy
+++ vars/mavenReleaseTag.groovy
@@ -1,10 +1,11 @@
 def call(MVN_DEV_VERSION, MVN_RELEASE_VERSION, gitbranch, GIT_PASS)
      {
           sh '''
-		git clone https://github.com/anandjainsa/scripts.git /tmp/releasescripts
+#		git clone https://github.com/anandjainsa/scripts.git /tmp/releasescripts
+		git clone git@github.com:anandjainsa/scripts.git /tmp/releasescripts
 		sleep 5
 		export GIT_PASSWORD="${GIT_PASS}"
-#		export GIT_ASKPASS="/tmp/releasescripts/gitpass.sh"
+		export GIT_ASKPASS="/tmp/releasescripts/gitpass.sh"
  
 		if [ -z ${MVN_DEV_VERSION} ] || [ -z ${MVN_RELEASE_VERSION} ]
  
