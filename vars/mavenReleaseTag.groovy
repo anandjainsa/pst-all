b0a22993475d90b8537519c71ff5f3c41c279296
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-04-25 12:01:43 +0530

    Update mavenReleaseTag.groovy

--- vars/mavenReleaseTag.groovy
+++ vars/mavenReleaseTag.groovy
@@ -16,7 +16,8 @@ def call(gitbranch, GIT_PASS)
                 fi
                 git clone git@github.com:anandjainsa/scripts.git /tmp/releasescripts
 
-                MVN_RELEASE_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout`
+                MVN_RELEASE_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout | cut -d"-" -f1`
+
                 sh /tmp/releasescripts/gitrelease.sh ${MVN_RELEASE_VERSION} ${gitbranch}
         '''
 }
