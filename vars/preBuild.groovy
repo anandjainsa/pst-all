20b07272d5ee29b844da71e8fd8f8dd2cd307988
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-04-25 12:00:56 +0530

    Update preBuild.groovy

--- vars/preBuild.groovy
+++ vars/preBuild.groovy
@@ -1,7 +1,7 @@
 def call()
      {
           sh '''
-                MVN_RELEASE_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout`
+                MVN_RELEASE_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout | cut -d"-" -f1`
 		git checkout -b develop
 		if [ ! -z $MVN_RELEASE_VERSION ]
 				then
